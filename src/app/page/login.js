95af93d5190f0c4a46650af221c41810b76d2536
JK <kabiljayaraman@gmail.com>
2024-06-23 08:52:17 +0400

    Update login.js
    fix

--- src/app/page/login.js
+++ src/app/page/login.js
@@ -37,8 +37,7 @@ export default function Login() {
       "Password": password
     };
     console.log(requestData)
-    const response =  await axios.post('/api/Member/ValidateUser', requestData, {headers: {'Content-Type': 'application/json',
-    }});
+    const response =  await axios.post(`/api/Member/ValidateUser?email=${email}&password=${password}`);
    
     console.log(response)
     if(response.status === HttpStatusCode.Ok) {
